f7fcc62d9cc8a0cb0cf24fd81b7babad8ff63cb8
Yaroslav Kulpan <yaroslav.kylpan@gmail.com>
2024-10-09 23:23:13 +0300

    fix build script

--- scripts/build-scripts/start-api.js
+++ scripts/build-scripts/start-api.js
@@ -1,4 +1,6 @@
 const { execSync } = require('child_process');
+const path = require('path');
+const fs = require('fs');
 
 // Function to run a shell command and log its output
 function runCommand(command) {
@@ -6,10 +8,23 @@ function runCommand(command) {
     const output = execSync(command, { stdio: 'inherit' }); // 'inherit' allows us to see the command's output in the console
     return output;
   } catch (error) {
-    console.error(`Failed to execute command: ${command}`, error);
+    console.error(`Failed to execute command: ${command}`);
+    console.error(`Error status: ${error.status}`);
+    console.error(`Error message: ${error.message}`);
+    console.error(`Stderr: ${error.stderr}`);
     process.exit(1); // Exit script with error code 1
   }
 }
 
+function verifyDirectoryExists(directoryPath) {
+  if (!fs.existsSync(directoryPath)) {
+    console.error(`Directory does not exist: ${directoryPath}`);
+    process.exit(1); // Exit script with error code 1
+  }
+}
+
+const apiDir = path.join(__dirname, './apps/api');
+verifyDirectoryExists(apiDir);
+
 console.log('Starting production server...');
-runCommand('cd ./apps/api && npm run start');
+runCommand(`cd ${apiDir} && npm run start`);
